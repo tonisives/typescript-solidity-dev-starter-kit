4850d63159dec4064955aa7e1b7d4a099226ec0e
tonis <tonisives@gmail.com>
2022-07-20 17:07:55 +0300

    use solidity 0.8.7

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,14 +19,14 @@ type HardhatUserEtherscanConfig = HardhatUserConfig & Etherscan;
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const RINKEBY_PRIVATE_KEY =
-  process.env.RINKEBY_PRIVATE_KEY! ||
+  process.env.TEST_DEPLOYMENT_KEY! ||
   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserEtherscanConfig = {
   defaultNetwork: "hardhat",
   solidity: {
-    compilers: [{ version: "0.7.6", settings: {} }],
+    compilers: [{ version: "0.8.7", settings: {} }],
   },
   networks: {
     hardhat: {},
